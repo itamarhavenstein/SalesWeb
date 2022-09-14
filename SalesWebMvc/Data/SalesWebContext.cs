1ba9681715fa8f16b04d1cf6c3b27240a9255f20
ItamarHavenstein <itamarhavenstein@hotmail.com>
2022-09-13 21:38:24 -0300

    adicionado novas entidades e feito a nova migração

--- SalesWebMvc/Data/SalesWebContext.cs
+++ SalesWebMvc/Data/SalesWebContext.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Models
 {
-    public class SalesWebContext : DbContext
+  public class SalesWebContext : DbContext
+  {
+    public SalesWebContext(DbContextOptions<SalesWebContext> options)
+        : base(options)
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
     }
+
+    public DbSet<Department> Department { get; set; }
+    public DbSet<Seller> Seller { get; set; }
+    public DbSet<SalesRecord> SalesRecord { get; set; }
+  }
 }
