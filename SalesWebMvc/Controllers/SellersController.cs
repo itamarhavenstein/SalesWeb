6703daf8f558a0d67e73a67ffe0f3961225765d3
ItamarHavenstein <itamarhavenstein@hotmail.com>
2022-09-17 18:15:52 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -54,20 +56,55 @@ namespace SalesWebMvc.Controllers
     [ValidateAntiForgeryToken]
     public IActionResult Delete(int id)
     {
-        _sellerService.Remove(id);
-        return RedirectToAction(nameof(Index));
+      _sellerService.Remove(id);
+      return RedirectToAction(nameof(Index));
     }
 
-    public IActionResult Details(int id){
+    public IActionResult Details(int? id)
+    {
       if (id == null)
         return NotFound();
 
-      var obj = _sellerService.FindById(id);
+      var obj = _sellerService.FindById(id.Value);
 
       if (obj == null)
         return NotFound();
 
       return View(obj);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+      if (id == null)
+        return NotFound();
+
+      var obj = _sellerService.FindById(id.Value);
+
+      if (obj == null)
+        return NotFound();
+
+      List<Department> departments = _departmentService.FindAll();
+      SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+      return View(viewModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Seller seller)
+    {
+      if (id != seller.Id)
+        return BadRequest();
+      try
+      {
+        _sellerService.Update(seller);
+        return RedirectToAction(nameof(Index));
+      }
+      catch (NotFoundException)
+      {
+        return NotFound();
+      }catch(DbConcurrencyException){
+        return BadRequest();
+      }
     }
   }
 }
