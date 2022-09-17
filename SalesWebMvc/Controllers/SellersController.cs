1c87f1cb12bfb0aea0b97b6dd851f9d6ba0599bb
ItamarHavenstein <itamarhavenstein@hotmail.com>
2022-09-17 17:08:06 -0300

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -25,7 +25,7 @@ namespace SalesWebMvc.Controllers
     public IActionResult Create()
     {
       var departments = _departmentService.FindAll();
-      var viewModel = new SellerFormViewModel{Departments = departments};
+      var viewModel = new SellerFormViewModel { Departments = departments };
       return View(viewModel);
     }
 
@@ -35,6 +35,27 @@ namespace SalesWebMvc.Controllers
     {
       _sellerService.Insert(seller);
       return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Delete(int? id)
+    {
+      if (id == null)
+        return NotFound();
+
+      var obj = _sellerService.FindById(id.Value);
+
+      if (obj == null)
+        return NotFound();
+
+      return View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+        _sellerService.Remove(id);
+        return RedirectToAction(nameof(Index));
     }
   }
 }
